5122437b81b2f64e0bed1ae346bab6080f744ea9
biofimex <biofimex@gmail.com>
2023-10-05 08:51:52 -0600

    ssss

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-ScrollReveal().reveal('.comidas', { delay: 500 });
+ScrollReveal().reveal('.comidas', { delay: 250 });
 ScrollReveal().reveal('.suplementos', { delay: 1000 });
-ScrollReveal().reveal('.planes', { delay: 1500 });
+ScrollReveal().reveal('.planes', { delay: 2100 });
+
+ScrollReveal().reveal('.cesar', { delay: 700 });
+ScrollReveal().reveal('.papas', { delay: 900 });
+ScrollReveal().reveal('.frutas', { delay: 1100 });
+
+ScrollReveal().reveal('.pre', { delay: 1300 });
+ScrollReveal().reveal('.prote', { delay: 1500 });
+ScrollReveal().reveal('.argi', { delay: 1700 });
+ScrollReveal().reveal('.creati', { delay: 1900 });
+
+ScrollReveal().reveal('.reloj', { delay: 2300 });
