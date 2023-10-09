e124abe8511ec48b405be7c12fc13e26195a3f81
biofimex <biofimex@gmail.com>
2023-10-09 09:33:13 -0600

    sssss

---
+++ ejercicios/biceps/ejercicios.js
@@ -0,0 +1,15 @@
+// Función para actualizar el contador y redirigir
+function actualizarContador() {
+    var contadorElement = document.getElementById("contador");
+    var valor = parseInt(contadorElement.innerText);
+
+    if (valor > 0) {
+        contadorElement.innerText = (valor - 1).toString();
+    } else {
+        // Redirige a pantalla2.html cuando el contador llega a 0
+        window.location.href = "pantalla2.html";
+    }
+}
+
+// Actualiza el contador cada segundo
+setInterval(actualizarContador, 1000);
